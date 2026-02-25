51dd843c141b922323d97677a0d4c3cc0d22226d
mdfkmw <madafaka_mw@yahoo.com>
2026-02-25 10:49:52 +0200

    Delete soferapp/ui directory

--- soferapp/ui/data/DriverTicket.kt
+++
@@ -1,9 +0,0 @@
-package ro.priscom.sofer.ui.models
-
-data class DriverTicket(
-    val destination: String,
-    val grossPrice: Double,
-    val finalPrice: Double,
-    val quantity: Int,
-    val discountPercent: Double
-)
